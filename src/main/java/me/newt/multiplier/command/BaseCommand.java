6166b6340973e33de9bfc990436c0ce32cd65ff2
TheBlockBender <floris.jolink@gmail.com>
2020-08-24 16:58:40 +0200

    [WIP] Update 1.16.1 - Completed command functionality.

--- src/main/java/me/newt/multiplier/command/BaseCommand.java
+++ src/main/java/me/newt/multiplier/command/BaseCommand.java
@@ -1,26 +1,27 @@
 package me.newt.multiplier.command;
 
 import me.newt.multiplier.MultiplierPlugin;
+import me.newt.multiplier.MultiplierType;
 import me.newt.multiplier.command.subcommands.admin.*;
 import me.newt.multiplier.command.subcommands.user.MenuCommand;
 import me.newt.multiplier.command.subcommands.user.ThankCommand;
+import me.newt.multiplier.messages.MessagesAPI;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.hover.content.Text;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
+import java.util.*;
 import java.util.logging.Level;
 
-public class BaseCommand implements CommandExecutor {
+public class BaseCommand implements CommandExecutor, TabCompleter {
 
-    private final MultiplierPlugin multiplierPlugin;
+    private final MessagesAPI msg;
     private final SubCommand menuCMD;
     private final SubCommand thankCMD;
     private final SubCommand giveCMD;
@@ -35,7 +36,7 @@ public class BaseCommand implements CommandExecutor {
      * @param multiplierPlugin Instance of the main class.
      */
     public BaseCommand(MultiplierPlugin multiplierPlugin) {
-        this.multiplierPlugin = multiplierPlugin;
+        this.msg = multiplierPlugin.getMessagesAPI();
 
         // User subcommands:
         this.menuCMD = new MenuCommand(multiplierPlugin, "multiplier.user.activate");
@@ -55,6 +56,7 @@ public class BaseCommand implements CommandExecutor {
             return;
         }
         command.setExecutor(this);
+        command.setTabCompleter(this);
     }
 
     /**
@@ -158,25 +160,25 @@ public class BaseCommand implements CommandExecutor {
      * @param label  Alias of the command used.
      */
     private void printHelp(CommandSender sender, String label) {
-        sender.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "---------------------------------------------------------------");
+        sender.sendMessage(msg.fullLine);
         if (sender.hasPermission(menuCMD.getPermission()))
-            print(sender, "/" + label, "View and activate your multipliers.");
+            print(sender, "/" + label, null, msg.get("command_description_menu"));
         if (sender.hasPermission(thankCMD.getPermission()))
-            print(sender, "/" + label + " thank", "Show your gratitude for a multiplier.");
+            print(sender, "/" + label + " thank", null, msg.get("command_description_thank"));
         if (sender.hasPermission(listCMD.getPermission()))
-            print(sender, "/" + label + " admin list <player>", "List a player's multipliers.");
+            print(sender, "/" + label + " admin list (...)", "/" + label + " admin <player>", msg.get("command_description_list"));
         if (sender.hasPermission(giveCMD.getPermission()))
-            print(sender, "/" + label + " admin give <player> <type> <duration> <strength>", "Give a multiplier.");
+            print(sender, "/" + label + " admin give (...)", "/" + label + " admin give <player> <type> <duration> <strength>", msg.get("command_description_give"));
         if (sender.hasPermission(removeCMD.getPermission()))
-            print(sender, "/" + label + " admin remove <id>", "Remove a multiplier.");
+            print(sender, "/" + label + " admin remove (...)", "/" + label + " admin remove <id>", msg.get("command_description_remove"));
         if (sender.hasPermission(startCMD.getPermission()))
-            print(sender, "/" + label + " admin start <type> <duration> <strength>", "Start a multiplier.");
+            print(sender, "/" + label + " admin start (...)", "/" + label + " admin start <type> <duration> <strength>", msg.get("command_description_start"));
         if (sender.hasPermission(stopCMD.getPermission()))
-            print(sender, "/" + label + " admin stop <type>", "Stop active multipliers.");
+            print(sender, "/" + label + " admin stop (...)", "/" + label + " admin stop <type>", msg.get("command_description_stop"));
         if (sender.hasPermission(reloadCMD.getPermission()))
-            print(sender, "/" + label + " admin reload", "Reload configuration files.");
-        print(sender, "/" + label + " help", "View this list of commands.");
-        sender.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "---------------------------------------------------------------");
+            print(sender, "/" + label + " admin reload", null, msg.get("command_description_reload"));
+        print(sender, "/" + label + " help", null, msg.get("command_description_help"));
+        sender.sendMessage(msg.fullLine);
     }
 
     /**
@@ -185,7 +187,7 @@ public class BaseCommand implements CommandExecutor {
      * @param command     Command that should be described.
      * @param description Description of the command.
      */
-    private void print(CommandSender sender, String command, String description) {
+    private void print(CommandSender sender, String command, String fullCommand, String description) {
 
         if (!(sender instanceof Player)) {
             sender.sendMessage(command + " - " + description);
@@ -193,20 +195,85 @@ public class BaseCommand implements CommandExecutor {
         }
 
         Player player = (Player) sender;
-        Text hoverText = new Text(ChatColor.GRAY + "Click to copy this command to your chat.");
+        Text hoverText = new Text(msg.get("command_copy_command"));
         HoverEvent onHover = new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText);
-        ClickEvent onClick = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command);
+        ClickEvent onClick;
+        if (fullCommand == null) {
+            onClick = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command);
+        } else {
+            onClick = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, fullCommand);
+        }
 
         BaseComponent[] message = new ComponentBuilder(command)
-                .color(ChatColor.GREEN)
+                .color(msg.primaryColor)
                 .event(onHover)
                 .event(onClick)
-                .append(" - ")
-                .color(ChatColor.GRAY)
+                .append(" ")
                 .reset()
                 .append(description)
+                .color(ChatColor.GRAY)
                 .create();
 
         player.spigot().sendMessage(message);
+    }
+
+    /**
+     * TabComplete handler.
+     * @param sender  Issuer of the command.
+     * @param command Executed command.
+     * @param label   Alias of the command used.
+     * @param args    Arguments used with the command.
+     * @return List of tab completions.
+     */
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            completions.add("");
+            completions.add("help");
+            if (sender.hasPermission(thankCMD.getPermission())) completions.add("thank");
+            if (sender.hasPermission(giveCMD.getPermission())
+                    || sender.hasPermission(listCMD.getPermission())
+                    || sender.hasPermission(reloadCMD.getPermission())
+                    || sender.hasPermission(removeCMD.getPermission())
+                    || sender.hasPermission(startCMD.getPermission())
+                    || sender.hasPermission(stopCMD.getPermission())) {
+                completions.add("admin");
+            }
+        }
+        if (args.length == 2 && args[0].equalsIgnoreCase("admin")) {
+            if (sender.hasPermission(giveCMD.getPermission())) completions.add("give");
+            if (sender.hasPermission(listCMD.getPermission())) completions.add("list");
+            if (sender.hasPermission(reloadCMD.getPermission())) completions.add("reload");
+            if (sender.hasPermission(removeCMD.getPermission())) completions.add("remove");
+            if (sender.hasPermission(startCMD.getPermission())) completions.add("start");
+            if (sender.hasPermission(stopCMD.getPermission())) completions.add("stop");
+        }
+
+        if (args.length > 1) {
+            if (args[0].equalsIgnoreCase("admin")) {
+                if (args[1].equalsIgnoreCase("give")) {
+                    if (args.length == 3)
+                        Bukkit.getOnlinePlayers().forEach(player -> completions.add(player.getName()));
+                    if (args.length == 4)
+                        EnumSet.allOf(MultiplierType.class).forEach(type -> completions.add(type.getCapitalizedName()));
+                }
+                if (args[1].equalsIgnoreCase("list")) {
+                    if (args.length == 3)
+                        Bukkit.getOnlinePlayers().forEach(player -> completions.add(player.getName()));
+                }
+                if (args[1].equalsIgnoreCase("start")) {
+                    if (args.length == 3)
+                        EnumSet.allOf(MultiplierType.class).forEach(type -> completions.add(type.getCapitalizedName()));
+                }
+                if (args[1].equalsIgnoreCase("stop")) {
+                    if (args.length == 3)
+                        EnumSet.allOf(MultiplierType.class).forEach(type -> completions.add(type.getCapitalizedName()));
+                }
+            }
+        }
+        Collections.sort(completions);
+        return completions;
     }
 }
